b72ebb0e8efc10f5056e7a5ab8eed5fca7d6e1b4
GauraVashisth <miya.musashi.moto1645@gmail.com>
2025-05-20 23:16:37 +0530

    basic maths: reverse-number

--- 00basics/basic-maths/palindrome-num.cpp
+++ 00basics/basic-maths/palindrome-num.cpp
@@ -1,7 +1,6 @@
 #include <cstdio>
-// bool palindrome(int &num) {
-void palindrome(int &num) {
-  int dup = num;
+bool palindrome(int &num, int &dup) {
+  // void palindrome(int &num, int &dup) {
   int revNum = 0;
   while (num > 0) {
     int rem = num % 10;
@@ -9,22 +8,23 @@ void palindrome(int &num) {
     revNum = (revNum * 10) + rem;
   }
   if (dup == revNum)
-    // return true;
-    printf("true\n");
+    return true;
+  // printf("true\n");
   else
-    // return false;
-    printf("false\n");
+    return false;
+  // printf("false\n");
 }
 
 int main() {
   int num;
+  int dup = num;
   printf("enter a number, check palindrome: ");
   scanf("%d", &num);
 
-  // if (palindrome(num))
-  //   printf("given number %d is a palindrome", num);
-  // else
-  //   printf("%d is not a palindrome", num);
-  palindrome(num);
+  if (palindrome(num, dup))
+    printf("given number %d is a palindrome\n", num);
+  else
+    printf("%d is not a palindrome\n", num);
+  // palindrome(num, dup);
   return 0;
 }
