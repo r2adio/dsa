a6a5b0020e5d97f490243f885cba32b2aff58618
GauraVashisth <miya.musashi.moto1645@gmail.com>
2025-06-04 18:30:51 +0530

    basic maths, reverse-number{leetcode}

--- 00basics/basic-maths/count-digits.cpp
+++ 00basics/basic-maths/count-digits.cpp
@@ -1,15 +1,32 @@
+// count digits: counts the number of digits in given number
+
+#include <cmath>
 #include <cstdio>
-void show_digit(int &num) {
-  std::printf("%s", "all the digits of a num: ");
+
+// Time Complexity: O(log10 N+1)
+// Space Complexity: O(1)
+int bruteforce(int &num) {
+  // std::printf("%s", "all the digits of a num: ");
   int count = 0;
   while (num > 0) {
-    int rem = num % 10;
-    std::printf("%d ", rem);
+    // int rem = num % 10;
+    // std::printf("%d ", rem);
     num = num / 10;
     count++;
   }
-  std::printf("\n");
-  std::printf("no. of digits in a number is: %d\n", count);
+  return count;
+}
+
+// Time Complexity: O(1)
+// Space Complexity: O(1)
+int optimal(int &num) {
+  int count = log10(num) + 1;
+  if (num > 0) {
+    return count;
+  } else {
+    int count = log10(num * -1) + 1;
+    return count;
+  }
 }
 
 int main() {
@@ -17,6 +34,7 @@ int main() {
   std::printf("Enter a number: ");
   std::scanf("%d", &num);
 
-  show_digit(num);
+  // bruteforce(num);
+  std::printf("no. of digits in a number is: %d\n", optimal(num));
   return 0;
 }
--- 00basics/basic-maths/reverse-number.cpp
+++ 00basics/basic-maths/reverse-number.cpp
@@ -1,12 +1,20 @@
+// reverse number : generate the reverse of a given number
+
+#include <climits>
 #include <cstdio>
-int revserse_num(int &num) {
-  int revNum = 0;
-  while (num > 0) {
+
+// Time Complexity : O(log10 N + 1)
+// Space Complexity : O(1);
+int bruteforce(int &num) {
+  long long revNum = 0;
+  while (num != 0) {
     int rem = num % 10;
     revNum = (revNum * 10) + rem;
     num = num / 10;
   }
-  return revNum;
+  if (revNum > INT_MAX || revNum < INT_MIN)
+    return 0;
+  return static_cast<int>(revNum);
 }
 
 int main(void) {
@@ -14,6 +22,6 @@ int main(void) {
   printf("enter a number: ");
   scanf("%d", &num);
 
-  printf("reverse of number %d is: %d\n", num, revserse_num(num));
+  printf("reverse of number %d is: %d\n", num, bruteforce(num));
   return 0;
 }
