896eeb61fb72019e92250342fb08b3906e08ec60
GauraVashisth <miya.musashi.moto1645@gmail.com>
2025-06-23 23:46:45 +0530

    recursion, starting

---
+++ 00basics/recursion/recursion.cpp
@@ -0,0 +1,17 @@
+// NOTE : Recursion
+// when a function calls itself until a specified condition it met
+#include <cstdio>
+void count(int x) {
+  // base condition for recursion
+  if (x == 9) {
+    return;
+  }
+  printf("%d\n", x);
+  x++;
+  count(x);
+}
+
+int main() {
+  int x = 0;
+  count(x);
+}
