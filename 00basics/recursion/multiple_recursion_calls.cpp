c7e7f171c2c3922c8fdf5c8f9cb3c0da4d6fc460
GauraVashisth <miya.musashi.moto1645@gmail.com>
2025-07-01 21:50:49 +0530

    recursion, parameterised_functional and multiple_recursion_calls

---
+++ 00basics/recursion/multiple_recursion_calls.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+
+// Time Complexity: O(2^n), not exponential but near to that.
+// Space Complexity: O()
+int fibonacci(int n) {
+  if (n <= 1) {
+    return n;
+  }
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main(int argc, char *argv[]) {
+  int n;
+  printf("enter the index value of fibonacci series: ");
+  scanf("%d", &n);
+  printf("%d\n", fibonacci(n));
+  return 0;
+}
---
+++ 00basics/recursion/parameterised_functional.cpp
@@ -0,0 +1,39 @@
+// PRINT THE SUMMATION OF ALL N NO. (from n to 1)
+
+#include <cstdio>
+void parameterised_summation(int i, int sum) {
+  if (i < 1) {
+    printf("%d\n", sum);
+    return;
+  }
+  parameterised_summation(i - 1, sum + i);
+}
+
+int functional_summation(int n) {
+  if (n == 0) {
+    return 0;
+  }
+  return n + functional_summation(n - 1);
+}
+
+int main() {
+  // int sum = 0;
+  // int n;
+  // scanf("%d", &n);
+  // for (int i = 1; i <= n; i++) {
+  //   sum += i;
+  // }
+  // printf("%d\n", sum);
+
+  int n;
+  printf("enter a natural number: ");
+  scanf("%d", &n);
+  parameterised_summation(n, 0);
+  printf("%d\n", functional_summation(n));
+
+  // int prod = 1;
+  // for (int i = 1; i <= n; i++) {
+  //   prod *= i;
+  // }
+  // printf("%d\n", prod);
+}
